721f0652f3f2e4975c22d9740e974d736b9d53c6
ErikTaylorUWE <eirikur2.taylor@live.uwe.ac.uk>
2023-12-16 23:19:03 +0000

    final
    made the art

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(400, 500);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  
+  let colour={
+    r : 255,
+    g : 0,
+    b : 0
+  };
+
+  for (let i = 0; i < height-i; i = i+3) {
+    stroke(colour.r-i, colour.g, colour.b);
+    triangle(200,i, 400,height/2, 0,height/2);
+    triangle(200,height-i, 400,height/2, 0,height/2);
+  } 
 }
